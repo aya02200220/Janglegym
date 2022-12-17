a8e81edc60e3499b4a94de3038262ab8733012d6
Aya <pyon220@gmail.com>
2022-12-16 21:03:23 -0800

    deleted unused files

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-(function () {
-  console.log("sss");
-  $(".has-sub").hover(function () {
-    $(this).children(".sub").stop().slideToggle();
-  });
-});
-}
